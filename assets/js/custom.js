a1d362ea296994cd41d052e726b73284be14f329
unknown <you@example.com>
2022-02-10 15:44:02 +0600

    this is my commit

---
+++ assets/js/custom.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+    $('.on-silder').slick({
+        dots: false,
+        infinite: true,
+        speed: 600,
+        slidesToShow: 1,
+        adaptiveHeight: true
+      });
+   
+    $('.counter').counterUp({
+        delay: 10,
+        time: 2000
+    });
+    $('.video').venobox();
+
+  
+      $('.customer-slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              arrows:false,
+              dots:true,
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
+       /*================================
+    slicknav
+    ==================================*/
+
+    $('.header-wrap .nav_mobile_menu').slicknav({
+      label: '',
+      duration: 1000,
+      easingOpen: "easeOutBounce", //available with jQuery UI
+      prependTo:'.mobile_menu',
+      closedSymbol: '&#43;',
+      openedSymbol: '&#8722;'
+  });
+});
